df00b2d9dc9ff97ad09978d9c87391d25da3a506
Guillermo Eduardo Nanni <gnanni@sistran.com.ar>
2019-10-23 08:26:43 -0300

    'init;

---
+++ functions/index.js
@@ -0,0 +1,24 @@
+const functions = require('firebase-functions');
+
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+
+exports.helloWorld = functions.https.onRequest((request, response) => {
+    
+    response.send("Hello only!");
+
+});
+
+exports.helloWorldTwo = functions.https.onRequest((request, response) => {
+    
+    response.send("Hello 2!");
+
+});
+
+exports.helloWorldThird = functions.https.onRequest((request, response) => {
+    
+    response.send("Hello 3!");
+
+});
+
+
